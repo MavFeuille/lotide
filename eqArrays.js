f88af5a6def3feaade31f9de993810a2b25b8d99
MavFeuille <mavis.feuille@gmail.com>
2021-08-18 00:26:04 -0400

    lint correction

--- eqArrays.js
+++ eqArrays.js
@@ -19,11 +19,10 @@ const eqArrays = function (arr1, arr2) {
 };
 
 
-console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]))); // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)); // => true
+console.log(assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false)); // => false
 
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true)
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false)
+console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true)); // => true
+console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false)); // => false
 
 
-console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
