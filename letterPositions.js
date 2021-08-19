ab42daa73a56b58b3079f1325920fa1cdd460116
MavFeuille <mavis.feuille@gmail.com>
2021-08-18 23:40:06 -0400

    create function to return the index number representing its number of occurrences

---
+++ letterPositions.js
@@ -0,0 +1,38 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length)  {
+    return false;
+  }
+  
+  for (let i = 0; i < arr1.length; i++){
+    if (arr1[i] !== arr2[i]) {
+      return false
+    } 
+  } return true;
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+
+  for (let i = 0; i < sentence.length; i++) {
+    let letter = sentence[i];
+    if (letter !== " ") {
+      if (results[letter]) {
+        results[letter].push(i);
+      } else {
+        results[letter] = [i]
+      }
+    }
+  }
+  return results;
+};
+
+//test case
+console.log(letterPositions("hello"));
