d77c67a4e999026c83530f8ed30597b0664370c0
MavFeuille <mavis.feuille@gmail.com>
2022-03-23 23:37:47 -0400

    Correct test case

--- head.js
+++ head.js
@@ -11,6 +11,6 @@ const assertEqual = (actual, expected) => {
   }
 };
 
-assertEqual(head([5, 6, 7], 5));
+assertEqual(head([5, 6, 7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 module.exports = head;
