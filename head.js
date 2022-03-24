eda16aa9cb3a0eabe4755879675c99cdf2f42b3f
MavFeuille <mavis.feuille@gmail.com>
2022-03-23 23:28:50 -0400

    Remove previous solution

--- head.js
+++ head.js
@@ -1,14 +1,2 @@
-const head = function(array) {
-  return array[0];
-};
- 
 
-// // Original test code from head.js
-// assertEqual(head([5,6,7]), 5);
-// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-// assertEqual(head(["NO","oh",7]), 5);
-// assertEqual(head([]), undefined);
-
-
-//this is used for other files, do not delete
 module.exports = head;
